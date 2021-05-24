8140cbe3c1173ef134647fbb7ddbdc408d9e2e2e
Jacqueline Macedo <jacquelinemacedo.ml@gmail.com>
2021-05-23 21:37:36 -0300

    JsonFormat

--- tour_backend/src/main/kotlin/com/ascm/tour/entities/RespostaJson.kt
+++ tour_backend/src/main/kotlin/com/ascm/tour/entities/RespostaJson.kt
@@ -1,8 +1,7 @@
 package com.ascm.tour.entities
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING
 import java.util.*
 
-data class RespostaJson (
-        var message: String,
-        var date: Date
-        )
+data class RespostaJson(val message: String, @JsonFormat(shape = STRING, pattern = "dd/mm/yyyy") val date: Date)
